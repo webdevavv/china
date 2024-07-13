455f7e843a61994ffc620559e2f12abfc193a613
Anatoliy Vikulov <vikulovtolya38@gmail.com>
2024-07-13 17:53:43 +0800

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 const screenWidth = window.screen.width;
-if (screenWidth <= 1023) {
-  const body = document.body;
-  const burger = document.querySelector(".burger");
-  const burgerMenu = document.querySelector(".burger__menu");
-  const burgerMenuItem = document.querySelectorAll(
-    ".burger__menu > .header__nav > ul > li"
-  );
+const body = document.body;
+const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burger__menu");
+const burgerMenuItem = document.querySelectorAll(
+  ".burger__menu > .header__nav > ul > li"
+);
+if (screenWidth <= 1023 && burgerMenu) {
   burger.addEventListener("click", () => {
     burgerMenu.classList.toggle("hide");
     body.classList.toggle("no-scroll");
@@ -47,23 +47,40 @@ function myFunction() {
     alreadyExecuted = true;
   }
 }
+const myDiv = document.getElementById("scroll-target");
+if (myDiv) {
+  window.addEventListener("scroll", () => {
+    const divPosition = myDiv.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
 
-window.addEventListener("scroll", () => {
-  const myDiv = document.getElementById("scroll-target");
-  const divPosition = myDiv.getBoundingClientRect().top;
-  const windowHeight = window.innerHeight;
-
-  // Проверка, чтобы убедиться, что див видим
-  if (divPosition < windowHeight) {
-    myFunction(); // Вызываем функцию, когда мы доползли до нужного дива
-  }
-});
+    // Проверка, чтобы убедиться, что див видим
+    if (divPosition < windowHeight) {
+      myFunction(); // Вызываем функцию, когда мы доползли до нужного дива
+    }
+  });
+}
 
 $(document).ready(function () {
   $(".panel-heading").click(function () {
     $(this).toggleClass("in").next().slideToggle();
   });
 });
+
+const modal = document.getElementById("modal-form");
+if (modal) {
+  const modalBtn = document.querySelectorAll("#modal-btn");
+  const modalBtnClose = document.getElementById("modal-close");
+  modalBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.classList.add("open");
+      body.classList.add("no-scroll");
+    });
+  });
+  modalBtnClose.addEventListener("click", () => {
+    modal.classList.remove("open");
+    body.classList.remove("no-scroll");
+  });
+}
 
 var swiper = new Swiper(".feedback-slider", {
   slidesPerView: 2.3,
@@ -76,3 +93,44 @@ var swiper = new Swiper(".feedback-slider", {
     1024: { slidesPerView: 2.7, spaceBetween: 20 },
   },
 });
+var swiper = new Swiper(".error-slider", {
+  slidesPerView: 2.3,
+  spaceBetween: 20,
+  navigation: { nextEl: ".error-next", prevEl: ".error-prev" },
+  breakpoints: {
+    320: { slidesPerView: 1.1, spaceBetween: 10 },
+    500: { slidesPerView: 1.4, spaceBetween: 15 },
+    768: { slidesPerView: 1.4, spaceBetween: 20 },
+    1024: { slidesPerView: 2.2, spaceBetween: 20 },
+  },
+});
+
+const priceSlide = document.querySelectorAll(
+  ".price-slider > .swiper-wrapper > .swiper-slide"
+);
+
+if (priceSlide.length == 1) {
+  const priceSlider = document.querySelector(".price-slider");
+  const priceSlideBlock = document.querySelector(
+    ".price-slider > .swiper-wrapper > .swiper-slide > .price_block__item"
+  );
+  priceSlider.classList.add("single-slider");
+  priceSlideBlock.classList.add("single-slide-block");
+
+  var swiper = new Swiper(".price-slider", {
+    slidesPerView: 1,
+    centeredSlides: true,
+  });
+} else {
+  var swiper = new Swiper(".price-slider", {
+    slidesPerView: 2.3,
+    spaceBetween: 20,
+    navigation: { nextEl: ".feedback-next", prevEl: ".feedback-prev" },
+    breakpoints: {
+      320: { slidesPerView: 1.1, spaceBetween: 10 },
+      500: { slidesPerView: 1.4, spaceBetween: 15 },
+      768: { slidesPerView: 1.4, spaceBetween: 20 },
+      1024: { slidesPerView: 2, spaceBetween: 20 },
+    },
+  });
+}
